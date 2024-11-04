2d3b6c56307f7815b9066235200d81d99af2ff14
DoHuy <huydqph50651@gmail.com>
2024-11-04 10:29:31 +0700

    Cập nhật file env

--- Common/env.php
+++ Common/env.php
@@ -1,9 +1,9 @@
 <?php
 // Đường dẫn vào client(mặc định sẽ vào đây)
-define('BASE_URL', 'http://localhost/PHP_Dev/DA1-TKTW/');
+define('BASE_URL', 'http://localhost/DA1-TKTW/');
 
 // ĐƯỜNG dẫn vào admin
-define('BASE_URL_ADMIN', 'http://localhost/PHP_Dev/DA1-TKTW/Admins/');
+define('BASE_URL_ADMIN', 'http://localhost/DA1-TKTW/Admins/');
 
 define('DB_HOST', 'localhost');
 define('DB_PORT', 3306);
